15cfcb99391a85ae2b601ec7e658af138154428e
Fejmi <fejmiqaz@gmail.com>
2024-10-26 13:48:22 +0200

    Update Lab1_SLL.java

--- SLL/Lab1_SLL.java
+++ SLL/Lab1_SLL.java
@@ -1,5 +1,22 @@
-package Labs;
 
+/* You are given a single linked list with String nodes. Additionally, you are given one more integer L. You need to insert a new node with value "Outlier" before each node that contains a String with more than L characters in the list.
+
+Input: In the first line from the input the number of elements in the list is given - N, and then in the next N lines the elements themselves. In the last line, the integer L is given.
+
+Output: The list before and after the transformation
+EXAMPLE:
+INPUT:
+3
+Sky
+Blue
+Cloud
+5
+
+OUTPUT:
+Sky->Blue->Cloud
+Sky->Blue->Cloud
+
+*/
 import java.util.Scanner;
 
 public class Lab1_SLL<E> {
