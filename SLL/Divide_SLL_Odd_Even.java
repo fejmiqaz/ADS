b4fd7131790545f7bb3414439a149d79ddb2ad9b
Fejmi <fejmiqaz@gmail.com>
2024-10-30 18:58:03 +0100

    Update Divide_SLL_Odd_Even.java

--- SLL/Divide_SLL_Odd_Even.java
+++ SLL/Divide_SLL_Odd_Even.java
@@ -1,4 +1,11 @@
-package Labs;
+/* For a single linked list, you need to create another 2 lists such that one of them will have even elements from the original list and the other one will hold the odd elements from the original list.
+Input:
+10
+1 2 3 4 5 6 7 8 9 10
+Output:
+2->4->6->8->10
+1->3->5->7->9
+*/
 
 import java.util.Scanner;
 
