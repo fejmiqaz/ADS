6a444c6e42ed0777b3bb0f3db63934a677f324b2
Fejmi <fejmiqaz@gmail.com>
2024-11-06 11:20:25 +0100

    Add files via upload

---
+++ AlgorithmsTechniques/Lab3_AlgorithmTechniques_2.java
@@ -0,0 +1,43 @@
+package Labs.AlgorithmsTechniques;
+
+/* We are given N tasks with estimated completion time and the amount we can earn from that task. Write an algorithm that
+* finds the maximum earnings we can have for 40 hours. Note that we can have a partial earning from a partially completed task.
+* Input: The first number in the input is the number of tasks N, then in the next N lines are  the time needed for the task (in hours)
+* and the amount of money we can earn from it.
+* Output: The maximum amount we can earn in 40 hours.
+* Example:
+* 3
+* 10 60
+* 20 100
+* 30 120
+* Output:
+* 200
+* Example 2:
+* 1
+* 10 60
+* Output:
+* 60 */
+
+import java.util.Scanner;
+public class Lab3_AlgorithmTechniques_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int overallHours = 40;
+        double overallAmount = 0;
+        while(n>0){
+            int hours = sc.nextInt();
+            int amount = sc.nextInt();
+            if(hours <= overallHours){
+                overallAmount += amount;
+                overallHours -= hours;
+            }else{
+                overallAmount += amount * ((double) overallHours / hours);
+                overallHours = 0;
+            }
+            n--;
+        }
+        System.out.println((int)overallAmount);
+
+    }
+}
---
+++ AlgorithmsTechniques/Lab3_MergeSort.java
@@ -0,0 +1,81 @@
+package Labs.AlgorithmsTechniques;
+
+import java.util.Scanner;
+
+public class Lab3_MergeSort {
+
+    int INF = 1000000;
+
+    //Merging two sorted arrays [l, mid], [mid+1, r]
+    //The result is a new sorted array
+    void merge(int a[], int l, int mid, int r) {
+        int numel = r - l + 1;
+        int temp[] = new int[100]; //new array for temporary storing of elements
+        int i, j, k = 0;
+
+        i = l;
+        j = mid + 1;
+
+        while ((i <= mid) && (j <= r)) {
+            if (a[i] < a[j]) {
+                temp[k] = a[i];
+                i++;
+            } else {
+                temp[k] = a[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k] = a[i];
+            i++;
+            k++;
+        }
+
+        while (j <= r) {
+            temp[k] = a[j];
+            j++;
+            k++;
+        }
+
+        for (k = 0; k < numel; k++) {
+            a[l + k] = temp[k];
+//            System.out.println(temp[k]);
+        }
+//        System.out.println();
+    }
+
+    void mergesort(int a[], int l, int r) {
+        if (l == r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        mergesort(a, l, mid);
+        mergesort(a, mid + 1, r);
+        merge(a, l, mid, r);
+    }
+    void reverse(int a[], int n){
+        for(int i = n; i >= 0; i--){
+            System.out.print(a[i]);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = input.nextInt();
+        }
+        Lab3_MergeSort obj = new Lab3_MergeSort();
+        obj.mergesort(arr, 0, arr.length - 1);
+        obj.reverse(arr, arr.length - 1);
+
+
+    }
+}
