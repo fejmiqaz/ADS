f3425d86d9ecf50206daf65cbec116947145f481
Fejmi <fejmiqaz@gmail.com>
2024-11-06 18:17:10 +0100

    Update DLL_SumListOfLists.java

--- DLL/DLL_SumListOfLists.java
+++ DLL/DLL_SumListOfLists.java
@@ -1,5 +1,3 @@
-package Labs.DLL;
-
 /* You are given a list that has another list inside. Get the sum of the elements.
 * Input: Integer n that tells us how many lists there are. Integer m that that tells us how many elements there are in each list.
 * Output: The the sum of the sub-lists. */
@@ -20,15 +18,21 @@ public class DLL_SumListOfLists {
             list1.insertLast(list2);
         }
         DLLNode<DLL<Integer>> curr1 = list1.getFirst();
+        int overallSum = 0;
         int sum = 0;
+        int i = 0;
         while(curr1 != null){
             DLLNode<Integer> curr2 = curr1.element.getFirst();
             while(curr2 != null){
                 sum += curr2.element;
                 curr2 = curr2.succ;
             }
+            i++;
+            System.out.println("Sub-list: " + i + " sum: " + sum);
+            overallSum += sum;
+            sum = 0;
             curr1 = curr1.succ;
         }
-        System.out.println(sum);
+        System.out.println("Overall sum: " + overallSum);
     }
 }
